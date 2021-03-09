186106e4ab37fec48af25eae7d798178ea958f9a
ezramizrahi <mizrahi.ez@gmail.com>
2021-03-09 09:05:18 +0800

    Quick code comment fix

--- cypress/integration/plan-page/plan-page.spec.js
+++ cypress/integration/plan-page/plan-page.spec.js
@@ -33,39 +33,39 @@ describe('Select Plans', () => {
       // We will set the preference based on the domain name
       const preferences = url.includes('.com.au') ? preferencesAU : preferencesUS;
 
-      // check that the plan widget exists in the DOM and is visible
+      // Check that the plan widget exists in the DOM and is visible
       planPage.getPlanWidget().should('exist');
       planPage.getPlanWidget().should('be.visible');
 
-      // click through each preference on the page
+      // Click through each preference on the page
       cy.wrap(preferences).each((preference) => {
-        // check that each preference exists in the DOM and is visible
+        // Check that each preference exists in the DOM and is visible
         // before clicking on it
         planPage.selectYourPreference(preference.id).should('exist');
         planPage.selectYourPreference(preference.id).should('be.visible');
         planPage.selectYourPreference(preference.id).click({force: true});
-        // check that "data-test-is-active" is true after the element
+        // Check that "data-test-is-active" is true after the element
         // has been clicked
         planPage.selectYourPreference(preference.id).then(($preference) => {
           cy.get($preference).invoke('attr', 'data-test-is-active').should('contain', 'true');
         });
-        // clicks through all Number of People options
+        // Clicks through all Number of People options
         planPage.selectAllPeople();
-        // clicks through all Recipes per Week options
+        // Clicks through all Recipes per Week options
         planPage.selectAllRecipes();
-        // check that the plan configuration is not empty
+        // Check that the plan configuration is not empty
         // and log the value
         planPage.getPlanConfigurationAU().should('not.be.empty');
         planPage.getPlanConfigurationAU().then(($plan) => {
           cy.log($plan.text());
         });
-        // check that Price Per Serving is not empty
+        // Check that Price Per Serving is not empty
         // and log the price
         planPage.getPrice().should('not.be.empty');
         planPage.getPrice().then(($price) => {
           cy.log($price.text());
         });
-        // check that Shipping cost is not empty
+        // Check that Shipping cost is not empty
         // and log the shipping cost
         planPage.getShippingCost().should('not.be.empty');
         planPage.getShippingCost().then(($shipping) => {
